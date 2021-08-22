1e071878e9c77c1396d837af9b175a2d4f70756c
Natasha Camargo <72647109+NatashaCamargo@users.noreply.github.com>
2021-08-22 16:38:07 -0300

    Add files via upload

---
+++ unesting_lists.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 22 16:36:30 2021
+
+@author: na_ca
+"""
+
+def flatten(my_list):
+  result = []
+  for el in my_list:
+    if isinstance(el, list):
+      print("list found!")
+      flat_list = flatten(el)
+      result += flat_list
+    else:
+      result.append(el)
+  return result
+
+
+### reserve for testing...
+planets = ['mercury', 'venus', ['earth'], 'mars', [['jupiter', 'saturn']], 'uranus', ['neptune', 'pluto']]
+print(flatten(planets))
